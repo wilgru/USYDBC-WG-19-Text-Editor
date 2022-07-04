08a2b8f00f18c0e3c260c736b9d61b824f9a0b59
wilgru <william.gruszka@gmail.com>
2022-07-04 13:57:13 +1000

    added indexDb CRUD functions

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,27 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const jate = await openDB('jate', 1);
+  const tx = jate.transaction('jate', 'readwrite');
+
+  const store = tx.objectStore('jate');
+  const request = store.add({value: content});
+  const result = await request;
+
+  return result;
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const jate = await openDB('jate', 1);
+  const tx = jate.transaction('jate', 'readonly');
+
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+
+  return result;
+};
 
 initdb();
